ea7472e26b74fe05ad1f8c59db6a21ceb94de347
aleksander-piprek <108300872+aleksander-piprek@users.noreply.github.com>
2024-03-12 17:47:08 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ class Book
             this->isbn = new int(*isbn);
             this->genre = new std::string(*genre);
             this->availability = new bool(*availability);
-            std::cout << "Created book: " << *title << " by "<< *author << ". ISBN: " << *isbn << ". Genre: " << *genre << ". Availability: " << *availability << std::endl;
+            std::cout << "Created book: " << *title << " by "<< *author << " ISBN: " << *isbn << " Genre: " << *genre << " Availability: " << *availability << std::endl;
         }
         ~Book()
         {
@@ -28,6 +28,27 @@ class Book
         }
 };
 
+class Member
+{
+    std::string* name;
+    int* id;
+    bool* status;
+    public:
+        Member(std::string *name, int *id, bool *status)
+        {
+            this->name = new std::string(*name);
+            this->id = new int(*id);
+            this->status = new bool(*status);
+            std::cout << "New member: " << *name << " ID: " << *id << " Membership status: " << *status << std::endl;
+        }
+        ~Member()
+        {
+            delete this->name;
+            delete this->id;
+            delete this->status;
+        }
+};
+
 int main()
 {
     std::string _title = "Harry-Potter";
@@ -36,5 +57,11 @@ int main()
     std::string _genre = "Fantasy";
     bool _availability = true;
     auto HarryPotter = new Book(&_title, &_author, &_isbn, &_genre, &_availability);
+ 
+    std::string _name = "Aleksander Piprek";
+    int _id = 1;
+    bool _status = true;
+    auto AleksanderPiprek = new Member(&_name, &_id, &_status);
+
     return 1;
 }
