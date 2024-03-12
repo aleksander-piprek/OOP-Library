0c197f38db3790d9718bb460ae679fd612665150
aleksander-piprek <108300872+aleksander-piprek@users.noreply.github.com>
2024-03-12 18:19:58 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
 
-class Book
+class Library
+{
+    std::vector<Book*> bookList;
+    std::vector<Member*> memberList; 
+    public:
+        Library() 
+        {
+
+        }
+        ~Library() 
+        {
+
+        }
+        void addBook(std::string title, std::string author, int isbn, std::string genre, bool availability)
+        {
+            auto newBook = new Book(&title, &author, &isbn, &genre, &availability);
+            bookList.push_back(newBook);
+            std::cout << "Created book: " << title << " by "<< author << " ISBN: " << isbn << " Genre: " << genre << " Availability: " << availability << std::endl;
+        }
+        void removeBook()
+        {
+
+        }
+        void displayBooks()
+        {
+
+        }        
+        void addMember(std::string name, int id, bool status)
+        {
+            auto newMember = new Member(&name, &id, &status);
+            memberList.push_back(newMember);
+            std::cout << "New member: " << name << " ID: " << id << " Membership status: " << status << std::endl;
+        }
+        void removeMember()
+        {
+
+        }
+        void displayMembers()
+        {
+
+        }
+};
+
+class Book : public Library
 {
     std::string* title;
     std::string* author;
@@ -16,7 +60,6 @@ class Book
             this->isbn = new int(*isbn);
             this->genre = new std::string(*genre);
             this->availability = new bool(*availability);
-            std::cout << "Created book: " << *title << " by "<< *author << ". ISBN: " << *isbn << ". Genre: " << *genre << ". Availability: " << *availability << std::endl;
         }
         ~Book()
         {
@@ -28,13 +71,37 @@ class Book
         }
 };
 
+class Member : public Library
+{
+    std::string* name;
+    int* id;
+    bool* status;
+    public:
+        Member(std::string *name, int *id, bool *status)
+        {
+            this->name = new std::string(*name);
+            this->id = new int(*id);
+            this->status = new bool(*status);
+        }
+        ~Member()
+        {
+            delete this->name;
+            delete this->id;
+            delete this->status;
+        }
+};
+
 int main()
 {
-    std::string _title = "Harry-Potter";
-    std::string _author = "J.K. Rowling";
-    int _isbn = 1;
-    std::string _genre = "Fantasy";
-    bool _availability = true;
-    auto HarryPotter = new Book(&_title, &_author, &_isbn, &_genre, &_availability);
+    auto library = new Library;
+        
+    library->addBook("Harry-Potter", "J.K. Rowling", 1, "Fantasy", true);
+
+    library->addMember("Aleksander Piprek", 1, true);
+
+    library->displayBooks();
+
+    library->displayMembers();
+
     return 1;
 }
